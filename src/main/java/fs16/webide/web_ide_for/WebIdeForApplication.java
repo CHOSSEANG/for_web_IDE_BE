457cdf2828fc2fd48bb60c94afa3a080505f0cb7
CHOSSEANG <jng00153@gmail.com>
2025-12-08 05:54:00 +0900

    초기 Project 생성

---
+++ src/main/java/fs16/webide/web_ide_for/WebIdeForApplication.java
@@ -0,0 +1,13 @@
+package fs16.webide.web_ide_for;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebIdeForApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebIdeForApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fs16/webide/web_ide_for/WebIdeForApplicationTests.java
@@ -0,0 +1,13 @@
+package fs16.webide.web_ide_for;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebIdeForApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
