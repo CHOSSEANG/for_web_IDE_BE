c0dd58ca0a3b533b3c50aebc8636dd5249be9eea
CHOSSEANG <jng00153@gmail.com>
2025-12-17 23:16:27 +0900

    feat : Container 수정 > DTO 추가

---
+++ src/main/java/fs16/webide/web_ide_for/container/dto/ContainerUpdateRequest.java
@@ -0,0 +1,28 @@
+package fs16.webide.web_ide_for.container.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 컨테이너 수정 요청 DTO
+ */
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContainerUpdateRequest {
+    /**
+     * 사용자 ID
+     */
+    private Long userId;
+
+    /**
+     * 수정할 컨테이너 ID
+     */
+    private Long containerId;
+
+    /**
+     * 새로운 컨테이너 이름
+     */
+    private String name;
+}
---
+++ src/main/java/fs16/webide/web_ide_for/container/dto/ContainerUpdateResponse.java
@@ -0,0 +1,41 @@
+package fs16.webide.web_ide_for.container.dto;
+
+import java.time.LocalDateTime;
+
+import fs16.webide.web_ide_for.container.entity.Container;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 컨테이너 수정 응답 DTO
+ */
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ContainerUpdateResponse {
+
+    // 컨테이너 ID
+    private Long id;
+
+    // 컨테이너 이름
+    private String name;
+
+    // 소유자 ID
+    private Long userId;
+
+    // 생성 시간
+    private LocalDateTime createdAt;
+
+    // Container 엔티티로부터 응답 DTO를 생성하는 정적 팩토리 메서드
+    public static ContainerUpdateResponse from(Container container) {
+        return ContainerUpdateResponse.builder()
+                .id(container.getId())
+                .name(container.getName())
+                .userId(container.getUser().getId())
+                .createdAt(container.getCreatedAt())
+                .build();
+    }
+}
