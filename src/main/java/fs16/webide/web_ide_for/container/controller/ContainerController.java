d7a407089342b02bdae10ae99b6c47c582609c7b
CHOSSEANG <jng00153@gmail.com>
2025-12-17 23:07:45 +0900

    feat : Container 삭제 > Controller에 삭제 기능 추가

--- src/main/java/fs16/webide/web_ide_for/container/controller/ContainerController.java
+++ src/main/java/fs16/webide/web_ide_for/container/controller/ContainerController.java
@@ -2,6 +2,7 @@ package fs16.webide.web_ide_for.container.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import fs16.webide.web_ide_for.common.ApiResponse;
 import fs16.webide.web_ide_for.container.dto.ContainerCreateRequest;
 import fs16.webide.web_ide_for.container.dto.ContainerCreateResponse;
+import fs16.webide.web_ide_for.container.dto.ContainerDeleteRequest;
+import fs16.webide.web_ide_for.container.dto.ContainerDeleteResponse;
 import fs16.webide.web_ide_for.container.dto.ContainerFindRequest;
 import fs16.webide.web_ide_for.container.dto.ContainerFindResponse;
 import fs16.webide.web_ide_for.container.dto.ContainerListRequest;
@@ -69,5 +72,20 @@ public class ContainerController {
         ContainerFindRequest request = new ContainerFindRequest(userId, containerId);
         Container container = containerService.findContainer(request);
         return ApiResponse.success(ContainerFindResponse.from(container));
+    }
+
+    /**
+     * 특정 ID의 컨테이너를 삭제합니다.
+     * 
+     * @param userId 사용자 ID
+     * @param containerId 삭제할 컨테이너의 ID
+     * @return 삭제된 컨테이너 정보
+     */
+    @DeleteMapping("/{containerId}")
+    public ApiResponse<ContainerDeleteResponse> deleteContainer(@RequestParam Long userId, @PathVariable Long containerId) {
+        log.info("Container delete request received for container ID: {}", containerId);
+        ContainerDeleteRequest request = new ContainerDeleteRequest(userId, containerId);
+        Container container = containerService.deleteContainer(request);
+        return ApiResponse.success(ContainerDeleteResponse.from(container));
     }
 }
