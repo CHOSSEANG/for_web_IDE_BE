a019427474c8afa197f6ce207f760394f679d77f
CHOSSEANG <jng00153@gmail.com>
2025-12-17 14:27:44 +0900

    docs : User 테이블을 참고해서 Create Table sql문 변경

--- document/sql/20251217_createContainer.sql
+++ document/sql/20251217_createContainer.sql
@@ -1,6 +1,6 @@
 CREATE TABLE container (
                        id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                       user_id VARCHAR(255) NOT NULL UNIQUE,
+                       user_id BIGINT NOT NULL UNIQUE,
                        name VARCHAR(255),
                        created_at LocalDateTime NOT NULL default datetime('now')
 );
