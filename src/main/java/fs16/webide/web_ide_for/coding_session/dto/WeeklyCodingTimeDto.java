673b620f4cdeaba7eb7f5da4d52b2ba983f50ed9
napum <gkdmsl9933@naver.com>
2025-12-17 17:12:49 +0900

    feat : 코딩 시간 측정 dto 어노테이션 추가

--- src/main/java/fs16/webide/web_ide_for/coding_session/dto/WeeklyCodingTimeDto.java
+++ src/main/java/fs16/webide/web_ide_for/coding_session/dto/WeeklyCodingTimeDto.java
@@ -1,9 +1,15 @@
 package fs16.webide.web_ide_for.coding_session.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class WeeklyCodingTimeDto {
     private List<DailyCodingTimeDto> daily;
