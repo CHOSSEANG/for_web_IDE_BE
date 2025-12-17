201a78de3999b72896501a3a5094eadabdc2e2ed
CHOSSEANG <jng00153@gmail.com>
2025-12-17 21:41:14 +0900

    feat : Container 전체 조회 > ContainerRepository 생성

--- src/main/java/fs16/webide/web_ide_for/container/repository/ContainerRepository.java
+++ src/main/java/fs16/webide/web_ide_for/container/repository/ContainerRepository.java
@@ -1,5 +1,7 @@
 package fs16.webide.web_ide_for.container.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,11 @@ import fs16.webide.web_ide_for.container.entity.Container;
 @Repository
 public interface ContainerRepository extends JpaRepository<Container, Long> {
 
+    /**
+     * 특정 사용자의 모든 컨테이너를 조회합니다.
+     * 
+     * @param userId 사용자 ID
+     * @return 사용자가 소유한 컨테이너 목록
+     */
+    List<Container> findAllByUserId(Long userId);
 }
