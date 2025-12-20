0d1634d66e53d6f0e3b991afbb653eaa57093f54
napum <gkdmsl9933@naver.com>
2025-12-20 20:32:36 +0900

    feat : 유저 에러코드 추가

--- src/main/java/fs16/webide/web_ide_for/user/error/UserErrorCode.java
+++ src/main/java/fs16/webide/web_ide_for/user/error/UserErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.boot.logging.LogLevel;
 @RequiredArgsConstructor
 public enum UserErrorCode implements ErrorCode {
     USER_NOT_FOUND("USR-001", "해당 유저를 찾을 수 없습니다.", LogLevel.WARN),
-    EMAIL_NOT_VERIFIED("USR-002", "인증되지 않은 이메일입니다.", LogLevel.WARN);
+    EMAIL_NOT_VERIFIED("USR-002", "인증되지 않은 이메일입니다.", LogLevel.WARN),
+    USER_EXISTED("USR-003", "이미 초대된 유저입니다.", LogLevel.WARN);
 
     private final String code;
     private final String message;
