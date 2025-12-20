aeadefa3e4097aaae366acf698db22617f96a770
napum <gkdmsl9933@naver.com>
2025-12-20 19:06:23 +0900

    feat : 비동기 설정 추가

--- src/main/java/fs16/webide/web_ide_for/websocket/config/AsyncConfig.java
+++ src/main/java/fs16/webide/web_ide_for/websocket/config/AsyncConfig.java
@@ -1,9 +1,24 @@
 package fs16.webide.web_ide_for.websocket.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
 public class AsyncConfig {
+
+    @Bean(name = "taskExecutor") // @Async 기본 executor로 사용됨
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5); // 기본 스레드 수
+        executor.setMaxPoolSize(10); // 최대 스레드 수
+        executor.setQueueCapacity(500); // 큐 수용량
+        executor.setThreadNamePrefix("Async-"); // 스레드 이름 접두사
+        executor.initialize();
+        return executor;
+    }
 }
