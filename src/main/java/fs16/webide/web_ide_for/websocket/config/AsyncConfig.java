ff8d76fb173a768a2477256458f25d7353e3063b
napum <gkdmsl9933@naver.com>
2025-12-20 14:34:33 +0900

    feat : AsyncConfig 비동기 설정

---
+++ src/main/java/fs16/webide/web_ide_for/websocket/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package fs16.webide.web_ide_for.websocket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
