9876a0821a83d906249a35d963b4b19f724908b6
CHOSSEANG <jng00153@gmail.com>
2025-12-23 01:11:31 +0900

    docs : 에러 코드 한글로 번역

--- src/main/java/fs16/webide/web_ide_for/file/error/FileErrorCode.java
+++ src/main/java/fs16/webide/web_ide_for/file/error/FileErrorCode.java
@@ -9,14 +9,13 @@ import fs16.webide.web_ide_for.common.error.ErrorCode;
 @Getter
 @RequiredArgsConstructor
 public enum FileErrorCode implements ErrorCode {
-    FILE_NOT_FOUND("FILE-001", "File not found.", LogLevel.INFO),
-    DIRECTORY_NOT_FOUND("FILE-002", "Directory not found.", LogLevel.INFO),
-    INVALID_FILE_PATH("FILE-003", "Invalid file path.", LogLevel.WARN),
-    FILE_ALREADY_EXISTS("FILE-004", "File already exists.", LogLevel.WARN),
-    DIRECTORY_ALREADY_EXISTS("FILE-005", "Directory already exists.", LogLevel.WARN),
-    CONTAINER_NOT_FOUND("FILE-006", "Container not found.", LogLevel.INFO),
-    PERMISSION_DENIED("FILE-007", "Permission denied.", LogLevel.WARN),
-    DIRECTORY_CANNOT_INCLUDE_CONTENT("FILE-008","폴더에는 내용을 추가할 수 없습니다.", LogLevel.WARN);
+    FILE_NOT_FOUND("FILE-001", "해당 파일을 찾을 수 없습니다.", LogLevel.INFO),
+    DIRECTORY_NOT_FOUND("FILE-002", "해당 폴더를 찾을 수 없습니다.", LogLevel.INFO),
+    INVALID_FILE_PATH("FILE-003", "해당 경로를 찾을 수 없습니다.", LogLevel.WARN),
+    FILE_ALREADY_EXISTS("FILE-004", "중복된 파일이 있습니다.", LogLevel.WARN),
+    DIRECTORY_ALREADY_EXISTS("FILE-005", "중복된 폴더가 있습니다.", LogLevel.WARN),
+    PERMISSION_DENIED("FILE-006", "접근 권한이 없습니다.", LogLevel.WARN),
+    DIRECTORY_CANNOT_INCLUDE_CONTENT("FILE-007","폴더에는 내용을 추가할 수 없습니다.", LogLevel.WARN);
 
     private final String code;
     private final String message;
