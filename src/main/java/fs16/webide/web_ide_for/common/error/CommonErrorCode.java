8b8f1cdc15b1daa9dc89fb9175bea5d4aeacc1ca
CHOSSEANG <jng00153@gmail.com>
2025-12-23 09:26:48 +0900

    docs : 오타 수정

--- src/main/java/fs16/webide/web_ide_for/common/error/CommonErrorCode.java
+++ src/main/java/fs16/webide/web_ide_for/common/error/CommonErrorCode.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum CommonErrorCode implements ErrorCode{
-	SERVER_ERROR("SYS-001", "예상치 못한 오류가 발생했습니다..", LogLevel.ERROR),
+	SERVER_ERROR("SYS-001", "예상치 못한 오류가 발생했습니다.", LogLevel.ERROR),
 	BAD_REQUEST("SYS-002", "잘못된 요청입니다.", LogLevel.WARN),
 	UNAUTHORIZED("SYS-003", "인증되지 않은 접근입니다.", LogLevel.WARN),
 	FORBIDDEN("SYS-004", "접근 권한이 없습니다.", LogLevel.WARN),
