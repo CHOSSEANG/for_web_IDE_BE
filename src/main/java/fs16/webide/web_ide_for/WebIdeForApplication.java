d4cd35f7fd0e16ffa4cbd66ade9cdb8ef6b0153b
napum <gkdmsl9933@naver.com>
2025-12-29 02:09:13 +0900

    feat : dateTimeProviderRef

--- src/main/java/fs16/webide/web_ide_for/WebIdeForApplication.java
+++ src/main/java/fs16/webide/web_ide_for/WebIdeForApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@EnableJpaAuditing
+@EnableJpaAuditing(dateTimeProviderRef = "auditingDateTimeProvider")
 @SpringBootApplication
 @ComponentScan(basePackages = "fs16.webide.web_ide_for")
 public class WebIdeForApplication {
