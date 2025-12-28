991ee091e38ced484c35609c8c76ba1d793e7ba1
napum <gkdmsl9933@naver.com>
2025-12-29 02:40:42 +0900

    feat : EnableJpaAuditing 추가

--- src/main/java/fs16/webide/web_ide_for/WebIdeForApplication.java
+++ src/main/java/fs16/webide/web_ide_for/WebIdeForApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+@EnableJpaAuditing
 @SpringBootApplication
 @ComponentScan(basePackages = "fs16.webide.web_ide_for")
 public class WebIdeForApplication {
