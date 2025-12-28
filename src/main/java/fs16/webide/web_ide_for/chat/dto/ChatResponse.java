de593522716de9e7cd87487745f9f3b24601cb3a
napum <gkdmsl9933@naver.com>
2025-12-29 02:58:12 +0900

    feat : response 제발되라 제발

--- src/main/java/fs16/webide/web_ide_for/chat/dto/ChatResponse.java
+++ src/main/java/fs16/webide/web_ide_for/chat/dto/ChatResponse.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Getter
@@ -18,13 +17,9 @@ public class ChatResponse {
     private String message;
     private String createdAt; // 한국 시간 문자열
 
-    // 엔티티 Chat -> DTO
     public static ChatResponse fromEntity(Chat chat) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
-        // LocalDateTime을 바로 포맷, DB에 저장된 시간은 이미 LocalDateTime 기준
-        LocalDateTime createdAtKST = chat.getCreatedAt(); // DB에 LocalDateTime 저장
-        String formattedCreatedAt = createdAtKST.format(formatter);
+        String formattedCreatedAt = chat.getCreatedAt().format(formatter); // 변환 제거
 
         return new ChatResponse(
                 chat.getSender().getId(),
