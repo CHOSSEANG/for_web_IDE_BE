5391242b47535ee74f9fb0c16d3830bcb72503a0
napum <gkdmsl9933@naver.com>
2025-12-29 02:03:55 +0900

    feat : jpaconfig 추가

---
+++ src/main/java/fs16/webide/web_ide_for/websocket/config/JpaConfig.java
@@ -0,0 +1,21 @@
+package fs16.webide.web_ide_for.websocket.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing(dateTimeProviderRef = "auditingDateTimeProvider")
+public class JpaConfig {
+
+    @Bean
+    public DateTimeProvider auditingDateTimeProvider() {
+        // OffsetDateTime을 그대로 사용해서 createdAt에 들어가도록 설정
+        return () -> Optional.of(OffsetDateTime.now(ZoneOffset.UTC));
+    }
+}
