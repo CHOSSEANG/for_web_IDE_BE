7bc68a24c4a418f62edd8cf1ebaa9fbb4ac1fa11
napum <gkdmsl9933@naver.com>
2025-12-29 02:11:21 +0900

    feat : dateTimeProviderRef 제거

--- src/main/java/fs16/webide/web_ide_for/WebIdeForApplication.java
+++ src/main/java/fs16/webide/web_ide_for/WebIdeForApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-@EnableJpaAuditing(dateTimeProviderRef = "auditingDateTimeProvider")
 @SpringBootApplication
 @ComponentScan(basePackages = "fs16.webide.web_ide_for")
 public class WebIdeForApplication {
